3c629494b4109d042f21f76f36c40adfc111649a
yunlong <yunlong17568@qq.com>
2017-11-16 16:52:34 +0800

    添加入口类的描述

--- light-library/src/main/java/online/heyworld/lightandroid/LightAndroidApplicationLike.java
+++ light-library/src/main/java/online/heyworld/lightandroid/LightAndroidApplicationLike.java
@@ -6,7 +6,18 @@ import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
 /**
- * Created by Administrator on 2017/11/16.
+ * LightAndroidApplicationLike
+ *
+ * The entry class to use Light Library.
+ * Which allow user not modify their code about Application{@link android.app.Application}.
+ *
+ *
+ *
+ * 使用 Light Library 的入口类.
+ * 使用者可以不需要修改Application相关的代码.
+ *
+ * @author YunLong
+ * @version 0.1
  */
 
 public class LightAndroidApplicationLike {
@@ -15,6 +26,11 @@ public class LightAndroidApplicationLike {
         setupLeakCanary(application);
     }
 
+    /**
+     *
+     * @param application
+     * @return
+     */
     protected RefWatcher setupLeakCanary(Application application) {
         if (LeakCanary.isInAnalyzerProcess(application)) {
             return RefWatcher.DISABLED;
